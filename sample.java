0206635a7b2925c33b1e3d8ea6d1433796106a54
Mayurj5842 <jmayur121212@gmail.com>
2025-10-27 23:49:18 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,5 +1,7 @@
 public class {
   public static void main(String[]args){
-    System.out.println("hello Jenkins");
+    System.out.println("Jenkins is an open-source automation tool written in Java that helps developers build, test, and deploy applications automatically.
+
+It is mainly used for Continuous Integration (CI) and Continuous Deployment (CD) — also known as CI/CD.");
       }
 }
